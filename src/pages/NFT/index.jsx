2cc718c9554f8b7de5475cb2e3a28683fc10cb53
Ultra-tech-code <hizick27@gmail.com>
2022-10-05 04:43:47 +0100

    edited the core part of nft page

--- src/pages/NFT/index.jsx
+++ src/pages/NFT/index.jsx
@@ -102,19 +102,18 @@ const NFT = () => {
           <h1>Details About Your Address</h1>
 
           <form onSubmit={handleSubmit}>
+
+            <div id="addr-input">
+            <label for="inp" class="inp">
+            <input type="text" onChange={(e) => setSearchAddress(e.target.value)}/>
+            <span class="label">NFT </span>
+            <span class="focus-bg"></span>
+             </label>
+            </div>
+
             <div id="dropanddetails">
               <div className="dropdown">
-                <input
-                  type="text"
-                  onChange={(e) => setSearchAddress(e.target.value)}
-                />
-
-                <select
-                  onChange={(e) => setProtocol(e.target.value)}
-                  name=""
-                  id=""
-                  className="dropbtn"
-                >
+                <select onChange={(e) => setProtocol(e.target.value)} className="dropbtn">
                   <option value={"ethereum"}>Ethereum</option>
                   <option value={"bitcoin"}>Bitcoin</option>
                   <option value={"solana"}>Solana</option>
@@ -123,36 +122,10 @@ const NFT = () => {
                   <option value={"fantom"}>Fantom</option>
                   <option value={"polkadot"}>Polkadot</option>
                 </select>
-
-                <select
-                  onChange={(e) => setDeterminant(e.target.value)}
-                  name=""
-                  id=""
-                  className="dropbtn"
-                >
-                  <option value={"address"}>Address</option>
-                  <option value={"block_id"}>Block ID</option>
-                  <option value={"transaction_hash"}>Transaction Hah</option>
-                </select>
-                {/* <button onclick={() => setNetwork(true)} className="dropbtn">
-                Network
-              </button>
-
-              {
-                network && (
-                    <div id="myDropdown" className="dropdown-content">
-                        <a href="#">Solana</a>
-                        <a href="#">Avalanche</a>
-                        <a href="#">Alogrand</a>
-                        <a href="#">Fantom</a>
-                        <a href="#">Polkadot</a>
-                    </div>
-
-                )
-              } */}
               </div>
               <button type="submit" id="fetchdata">
-                {loading ? "Loading..." : "Get Details"}
+              {loading ? "Loading..." : "Get Details"}
+                
               </button>
             </div>
           </form>
@@ -180,32 +153,18 @@ const NFT = () => {
             result?.map((res, index) => {
               return (
                 <tr key={index}>
-                  <td>{res?.name?.slice(0, 30)}</td>
                   <td>
-                  {`${res?.token_id?.slice(0, 6)}...${res?.token_id.slice(
-                    -6
-                  )}`}
-                  
+                    {res?.name?.slice(0, 30)}
                   </td>
                   <td>
-                  {`${res?.contract_address?.slice(0, 6)}...${res?.contract_address.slice(
-                    -6
-                  )}`}
+                  {`${res?.token_id?.slice(0, 6)}...${res?.token_id.slice(-6)}`}
                   </td>
-
-
-
-
-                  {/* <td>
-                    {res?.status?.charAt(0).toUpperCase() + res.status.slice(1)}
-                  </td> */}
+                  <td>
+                  {`${res?.contract_address?.slice(0, 6)}...${res?.contract_address.slice(-6)}`}
+                  </td>
                   <td>
                     <img src={res?.image_url} alt="" />
                   </td>
-                  {/* <td>{res?.num_events}</td>
-                  <td>
-                    <button onClick={() => handleEvent(res?.id)}>Event</button>
-                  </td> */}
                 </tr>
               );
             })
@@ -221,108 +180,73 @@ const NFT = () => {
         contentLabel="Events"
       >
         <div style={{ color: "#000" }}>
-          <div className="flex justify-between mb-2">
-            <h2
-              className="text-2xl"
-              ref={(_subtitle) => (subtitle = _subtitle)}
-            >
+          <div>
+            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
               Events
             </h2>
             <button onClick={() => setShowModal(!showModal)}>
               <AiOutlineClose size={24} />
             </button>
           </div>
+          <div >
+            <table>
+              <thead>
+                <tr>
+                  <th>txn id </th>
+                  <th>from</th>
+                  <th>to</th>
+                  <th>amount</th>
+                  <th>denomination</th>
+                </tr>
+              </thead>
 
-          <div className="flex flex-col mt-2">
-            <div className="mb-3 overflow-x-auto rounded-xl shadow-md">
-              <div className="pt-2 align-middle inline-block min-w-full">
-                <div className="overflow-hidden border-gray-200 rounded-xl">
-                  <table className="w-full divide-y divide-gray-200 relative px-4">
-                    <thead className="bg-dark_deep">
-                      <tr>
-                        <th
-                          scope="col"
-                          className="px-1 md:px-3 py-5 text-xs font-medium text-white_variant uppercase tracking-wider"
-                        >
-                          txn id
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-2 md:px-4 py-5 text-left text-xs font-medium text-white_variant uppercase tracking-wider"
-                        >
-                          from
-                        </th>
-                        <th
-                          scope="col"
-                          className="md:px-3 px-1 py-5 text-left text-xs font-medium text-white_variant uppercase tracking-wider"
-                        >
-                          to
-                        </th>
-                        <th
-                          scope="col"
-                          className="md:px-3 px-1 py-5 text-left text-xs font-medium text-white_variant uppercase tracking-wider"
-                        >
-                          amount
-                        </th>
-                        <th
-                          scope="col"
-                          className="md:px-3 px-1 py-5 text-left text-xs font-medium text-white_variant uppercase tracking-wider"
-                        >
-                          denomination
-                        </th>
+              <tbody className="bg-white divide-y divide-gray-300">
+                {Array.isArray(selectedEvent) &&
+                selectedEvent.length === 0 ? (
+                  <tr className="mb-4">
+                    <td
+                      colSpan={7}
+                      className="font-medium whitespace-nowrap text-center px-1 md:px-3 py-3"
+                    >
+                      Transaction has no event!
+                    </td>
+                  </tr>
+                ) : (
+                  selectedEvent?.slice(0, 5)?.map((event, index) => {
+                    return (
+                      <tr className="mb-4" key={event.id}>
+                        <td className="whitespace-nowrap truncate md:px-4 px-2 py-3">
+                          {`${event?.transaction_id.slice(
+                            0,
+                            6
+                          )}...${event?.transaction_id.slice(-6)}`}
+                        </td>
+                        <td className="text-sm truncate whitespace-nowrap md:px-3 px-1 py-3">
+                          {`${event?.source.slice(
+                            0,
+                            9
+                          )}...${event?.source.slice(-9)}`}
+                        </td>
+                        <td className="text-sm truncate whitespace-nowrap md:px-3 px-1 py-3">
+                          {event?.type === "transfer"
+                            ? `${event?.destination.slice(
+                                0,
+                                9
+                              )}...${event?.destination.slice(-9)}`
+                            : "-"}
+                        </td>
+                        <td className="text-sm truncate whitespace-nowrap md:px-3 px-1 py-3">
+                          {(event?.amount / 1e18).toFixed(6)}
+                        </td>
+                        <td className="text-sm truncate whitespace-nowrap md:px-3 px-1 py-3">
+                          {event?.denomination}
+                        </td>
                       </tr>
-                    </thead>
-
-                    <tbody className="bg-white divide-y divide-gray-300">
-                      {Array.isArray(selectedEvent) &&
-                      selectedEvent.length === 0 ? (
-                        <tr className="mb-4">
-                          <td
-                            colSpan={7}
-                            className="font-medium whitespace-nowrap text-center px-1 md:px-3 py-3"
-                          >
-                            Transaction has no event!
-                          </td>
-                        </tr>
-                      ) : (
-                        selectedEvent?.slice(0, 5)?.map((event, index) => {
-                          return (
-                            <tr className="mb-4" key={event.id}>
-                              <td className="whitespace-nowrap truncate md:px-4 px-2 py-3">
-                                {`${event?.transaction_id.slice(
-                                  0,
-                                  6
-                                )}...${event?.transaction_id.slice(-6)}`}
-                              </td>
-                              <td className="text-sm truncate whitespace-nowrap md:px-3 px-1 py-3">
-                                {`${event?.source.slice(
-                                  0,
-                                  9
-                                )}...${event?.source.slice(-9)}`}
-                              </td>
-                              <td className="text-sm truncate whitespace-nowrap md:px-3 px-1 py-3">
-                                {event?.type === "transfer"
-                                  ? `${event?.destination.slice(
-                                      0,
-                                      9
-                                    )}...${event?.destination.slice(-9)}`
-                                  : "-"}
-                              </td>
-                              <td className="text-sm truncate whitespace-nowrap md:px-3 px-1 py-3">
-                                {(event?.amount / 1e18).toFixed(6)}
-                              </td>
-                              <td className="text-sm truncate whitespace-nowrap md:px-3 px-1 py-3">
-                                {event?.denomination}
-                              </td>
-                            </tr>
-                          );
-                        })
-                      )}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
+                    );
+                  })
+                )}
+              </tbody>
+            </table>
           </div>
         </div>
       </Modal>
